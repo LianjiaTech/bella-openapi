3d6aa66f0d417da8c2004e7bd09ec80269f91741
saizhuolin001 <saizhuolin001@ke.com>
2025-06-20 18:24:09 +0800

    feat: adapt native message api

--- api/server/src/main/java/com/ke/bella/openapi/protocol/message/MessageAdaptor.java
+++ api/server/src/main/java/com/ke/bella/openapi/protocol/message/MessageAdaptor.java
@@ -10,36 +10,12 @@ import com.ke.bella.openapi.protocol.completion.ToolCallSimulator;
 
 public interface MessageAdaptor<T extends CompletionProperty> extends IProtocolAdaptor {
 
-    CompletionAdaptor<T> delegator();
+    MessageResponse createMessages(MessageRequest request, String url, T property);
 
-    default MessageResponse createMessages (MessageRequest request, String url, T property, EndpointProcessData processData) {
-        CompletionAdaptor<T> delegator = decorateAdaptor(delegator(), property, processData);
-        CompletionResponse completionResponse = delegator.completion(TransferUtils.convertRequest(request ,isNativeSupport()), url, property);
-        processData.setResponse(completionResponse);
-        return TransferUtils.convertResponse(completionResponse);
-    }
-
-    default void streamMessages(MessageRequest request, String url, T property, EndpointProcessData processData, Callbacks.StreamCompletionCallback callback) {
-        CompletionAdaptor<T> delegator = decorateAdaptor(delegator(), property, processData);
-        delegator.streamCompletion(TransferUtils.convertRequest(request, isNativeSupport()), url, property, callback);
-    }
+    void streamMessages(MessageRequest request, String url, T property, Callbacks.StreamCompletionCallback callback);
 
     @Override
     default String endpoint() {
         return "/v1/messages";
     }
-
-    default CompletionAdaptor<T> decorateAdaptor(CompletionAdaptor<T> adaptor, CompletionProperty property, EndpointProcessData processData) {
-        if(property.isFunctionCallSimulate()) {
-            adaptor = new ToolCallSimulator<>(adaptor, processData);
-        }
-        return adaptor;
-    }
-
-    @Override
-    default Class<?> getPropertyClass() {
-        return delegator().getPropertyClass();
-    }
-
-    boolean isNativeSupport();
 }
