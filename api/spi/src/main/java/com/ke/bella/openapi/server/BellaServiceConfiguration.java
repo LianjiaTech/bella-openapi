4a8c3ec20c9b2c6c2360c6c3fe56fe2b307abced
saizhuolin001 <saizhuolin001@ke.com>
2025-09-01 18:25:58 +0800

    feat: update spi configuration

--- api/spi/src/main/java/com/ke/bella/openapi/server/BellaServiceConfiguration.java
+++ api/spi/src/main/java/com/ke/bella/openapi/server/BellaServiceConfiguration.java
@@ -10,11 +10,15 @@ import com.ke.bella.openapi.utils.HttpUtils;
 import com.theokanning.openai.client.OpenAiApi;
 import com.theokanning.openai.service.OpenAiService;
 import okhttp3.OkHttpClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import retrofit2.Retrofit;
 
 import javax.annotation.PostConstruct;
@@ -22,8 +26,14 @@ import java.util.concurrent.ExecutorService;
 
 @EnableConfigurationProperties(OpenapiProperties.class)
 @Configuration
-public class BellaServiceConfiguration {
+public class BellaServiceConfiguration implements WebMvcConfigurer {
 
+    @Autowired
+    @Lazy
+    private ConcurrentStartInterceptor concurrentStartInterceptor;
+    @Autowired
+    @Lazy
+    private AuthorizationInterceptor authorizationInterceptor;
 
     @PostConstruct
     public void init() {
@@ -65,5 +75,14 @@ public class BellaServiceConfiguration {
 
         ExecutorService executorService = client.dispatcher().executorService();
         return new OpenAiService(openAiApi, executorService);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authorizationInterceptor)
+                .addPathPatterns("/console/**")
+                .addPathPatterns("/v*/**")
+                .order(100);
+        registry.addInterceptor(concurrentStartInterceptor);
     }
 }
