9e723b295b0fa40a469735186acecd85b3908298
sunmingzhu <sunmingzhu@ke.com>
2025-10-15 10:50:53 +0800

    add test conf, update Authorization bear, update nginx conf,update docker conf, update start.sh, update rateLimiter.

--- api/server/src/main/java/com/ke/bella/openapi/protocol/limiter/LimiterManager.java
+++ api/server/src/main/java/com/ke/bella/openapi/protocol/limiter/LimiterManager.java
@@ -18,86 +18,86 @@ import java.util.List;
 @Component
 @Slf4j
 public class LimiterManager {
-    @Autowired
-    private LuaScriptExecutor executor;
+	@Autowired
+	private LuaScriptExecutor executor;
 
-    @Autowired
-    private RedissonClient redisson;
+	@Autowired
+	private RedissonClient redisson;
 
-    private static final String RPM_KEY_FORMAT = "bella-openapi-limiter-rpm:%s:%s";
-    private static final String RPM_COUNT_KEY_FORMAT = "bella-openapi-limiter-rpm-count:%s:%s";
-    private static final String CONCURRENT_KEY_FORMAT = "bella-openapi-limiter-concurrent:%s:%s";
+	private static final String RPM_KEY_FORMAT = "bella-openapi-limiter-rpm:%s:%s";
+	private static final String RPM_COUNT_KEY_FORMAT = "bella-openapi-limiter-rpm-count:%s:%s";
+	private static final String CONCURRENT_KEY_FORMAT = "bella-openapi-limiter-concurrent:%s:%s";
 
-    public void record(EndpointProcessData processData) {
-        if(processData.getChannelCode() == null) {
-            return;
-        }
-        String entityCode = processData.getModel() != null ? processData.getModel() : processData.getEndpoint();
-        String akCode = processData.getAkCode();
-        String requestId = processData.getRequestId();
-        if (entityCode == null || akCode == null) {
-            return;
-        }
-        long currentTimestamp = DateTimeUtils.getCurrentSeconds();
-        if(requestId != null) {
-            // RPM记录
-            incrementRequestCountPerMinute(akCode, entityCode, requestId, currentTimestamp);
-        }
+	public void record(EndpointProcessData processData) {
+		if(processData.getChannelCode() == null) {
+			return;
+		}
+		String entityCode = processData.getModel() != null ? processData.getModel() : processData.getEndpoint();
+		String akCode = processData.getAkCode();
+		String requestId = processData.getRequestId();
+		if (entityCode == null || akCode == null) {
+			return;
+		}
+		long currentTimestamp = DateTimeUtils.getCurrentSeconds();
+		if(requestId != null) {
+			// RPM记录
+			incrementRequestCountPerMinute(akCode, entityCode, requestId, currentTimestamp);
+		}
 
-        // 减少并发请求计数
-        decrementConcurrentCount(akCode, entityCode);
-    }
+		// 减少并发请求计数
+		decrementConcurrentCount(akCode, entityCode);
+	}
 
-    public void incrementRequestCountPerMinute(String akCode, String entityCode, String requestId, long currentTimestamp) {
-        String rpmKey = String.format(RPM_KEY_FORMAT, entityCode, akCode);
-        String countKey = String.format(RPM_COUNT_KEY_FORMAT, entityCode, akCode);
+	public void incrementRequestCountPerMinute(String akCode, String entityCode, String requestId, long currentTimestamp) {
+		String rpmKey = String.format(RPM_KEY_FORMAT, entityCode, akCode);
+		String countKey = String.format(RPM_COUNT_KEY_FORMAT, entityCode, akCode);
 
-        List<Object> keys = Lists.newArrayList(rpmKey, countKey);
-        List<Object> params = new ArrayList<>();
-        params.add(currentTimestamp);
-        params.add(requestId);
+		List<Object> keys = Lists.newArrayList(rpmKey, countKey);
+		List<Object> params = new ArrayList<>();
+		params.add(currentTimestamp);
+		params.add(requestId);
 
-        try {
-            executor.execute("/rpm", ScriptType.limiter, keys, params);
-        } catch (IOException e) {
-            log.warn(e.getMessage(), e);
-        }
-    }
+		try {
+			executor.execute("/rpm", ScriptType.limiter, keys, params);
+		} catch (IOException e) {
+			log.warn(e.getMessage(), e);
+		}
+	}
 
 
-    public Long getRequestCountPerMinute(String akCode, String entityCode) {
-        String countKey = String.format(RPM_COUNT_KEY_FORMAT, entityCode, akCode);
-        Object count = redisson.getBucket(countKey).get();
-        return count != null ? Long.parseLong(count.toString()) : 0L;
-    }
-    
-    public void incrementConcurrentCount(String akCode, String entityCode) {
-        String concurrentKey = String.format(CONCURRENT_KEY_FORMAT, entityCode, akCode);
-        List<Object> keys = Lists.newArrayList(concurrentKey);
-        List<Object> params = new ArrayList<>();
-        params.add("INCR");
-        try {
-            executor.execute("/concurrent", ScriptType.limiter, keys, params);
-        } catch (IOException e) {
-            log.warn(e.getMessage(), e);
-        }
-    }
-    
-    public void decrementConcurrentCount(String akCode, String entityCode) {
-        String concurrentKey = String.format(CONCURRENT_KEY_FORMAT, entityCode, akCode);
-        List<Object> keys = Lists.newArrayList(concurrentKey);
-        List<Object> params = new ArrayList<>();
-        params.add("DECR");
-        try {
-            executor.execute("/concurrent", ScriptType.limiter, keys, params);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    
-    public Long getCurrentConcurrentCount(String akCode, String entityCode) {
-        String concurrentKey = String.format(CONCURRENT_KEY_FORMAT, entityCode, akCode);
-        Object count = redisson.getBucket(concurrentKey).get();
-        return count != null ? Long.parseLong(count.toString()) : 0L;
-    }
+	public Long getRequestCountPerMinute(String akCode, String entityCode) {
+		String countKey = String.format(RPM_COUNT_KEY_FORMAT, entityCode, akCode);
+		Object count = redisson.getBucket(countKey).get();
+		return count != null ? Long.parseLong(count.toString()) : 0L;
+	}
+
+	public void incrementConcurrentCount(String akCode, String entityCode) {
+		String concurrentKey = String.format(CONCURRENT_KEY_FORMAT, entityCode, akCode);
+//        List<Object> keys = Lists.newArrayList(concurrentKey);
+//        List<Object> params = new ArrayList<>();
+//        params.add("INCR");
+//        try {
+//            executor.execute("/concurrent", ScriptType.limiter, keys, params);
+//        } catch (IOException e) {
+//            log.warn(e.getMessage(), e);
+//        }
+	}
+
+	public void decrementConcurrentCount(String akCode, String entityCode) {
+		String concurrentKey = String.format(CONCURRENT_KEY_FORMAT, entityCode, akCode);
+		List<Object> keys = Lists.newArrayList(concurrentKey);
+		List<Object> params = new ArrayList<>();
+		params.add("DECR");
+		try {
+			executor.execute("/concurrent", ScriptType.limiter, keys, params);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public Long getCurrentConcurrentCount(String akCode, String entityCode) {
+		String concurrentKey = String.format(CONCURRENT_KEY_FORMAT, entityCode, akCode);
+		Object count = redisson.getBucket(concurrentKey).get();
+		return count != null ? Long.parseLong(count.toString()) : 0L;
+	}
 }
