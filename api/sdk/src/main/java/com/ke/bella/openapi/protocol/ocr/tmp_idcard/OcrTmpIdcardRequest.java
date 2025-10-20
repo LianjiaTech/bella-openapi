6d1bf1e3e30ae2bf0126a3bd47b2bf185489dbe0
liudazhuang002 <liudazhuang002@ke.com>
2025-10-20 14:34:59 +0800

    temp idcard protocol

---
+++ api/sdk/src/main/java/com/ke/bella/openapi/protocol/ocr/tmp_idcard/OcrTmpIdcardRequest.java
@@ -0,0 +1,11 @@
+package com.ke.bella.openapi.protocol.ocr.tmp_idcard;
+
+import com.ke.bella.openapi.protocol.ocr.OcrRequest;
+
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@SuperBuilder
+@NoArgsConstructor
+public class OcrTmpIdcardRequest extends OcrRequest {
+}
