3f4cf38f5c2ff0f87ccb11641b1181c9eeb5581e
liudazhuang002 <liudazhuang002@ke.com>
2025-12-05 15:47:22 +0800

    ocr: ocr_host->api_host

--- web/src/config.ts
+++ web/src/config.ts
@@ -4,4 +4,3 @@ export const safety_apply_url = process.env.NEXT_PUBLIC_SAFETY_APPLY_URL
 export const agent_url = process.env.NEXT_PUBLIC_AGENT_URL
 export const workflow_playground = process.env.NEXT_PUBLIC_WORKFLOW_PLAYGROUND
 export const document_parse_playground = process.env.NEXT_PUBLIC_DOCUMENT_PARSE_PLAYGROUND
-export const ocr_host = process.env.NEXT_PUBLIC_OCR_HOST
