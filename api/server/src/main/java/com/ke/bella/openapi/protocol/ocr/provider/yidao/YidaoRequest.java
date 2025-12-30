10feb52b59b5fce57d28b624c4f311fa04335f34
liudazhuang002 <liudazhuang002@ke.com>
2025-12-30 15:56:40 +0800

    refactor: ocr extract common segment - YidaoAdaptor

--- api/server/src/main/java/com/ke/bella/openapi/protocol/ocr/provider/yidao/YidaoRequest.java
+++ api/server/src/main/java/com/ke/bella/openapi/protocol/ocr/provider/yidao/YidaoRequest.java
@@ -1,4 +1,4 @@
-package com.ke.bella.openapi.protocol.ocr;
+package com.ke.bella.openapi.protocol.ocr.provider.yidao;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
